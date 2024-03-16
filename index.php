ffdb9b54f3eb5451deaa086caa8c8bab26e617db
abdullohmmuttaqin <abdullohmmuttaqin.09@gmail.com>
2024-03-17 02:18:46 +0700

    menambahkan paragraf

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>coba git remote</title>
+</head>
+<body>
+    <h1>Hello Word<h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur deleniti nemo, vero voluptas, debitis quam impedit unde nihil assumenda nisi eos quis sapiente! Qui autem commodi expedita odit perspiciatis labore!<p>
+</body>
+</html>

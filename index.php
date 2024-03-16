e9e40ddd90964b60724b493ea5f07287357c20bd
abdullohmmuttaqin <abdullohmmuttaqin.09@gmail.com>
2024-03-17 03:05:37 +0700

    nambah list

--- index.php
+++ index.php
@@ -8,5 +8,7 @@
 <body>
     <h1>Hello Word<h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur deleniti nemo, vero voluptas, debitis quam impedit unde nihil assumenda nisi eos quis sapiente! Qui autem commodi expedita odit perspiciatis labore!<p>
+    <li>satu<li>
+    <li>dua<li>
 </body>
 </html>
